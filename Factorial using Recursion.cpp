e285202e8dcd1437ca85f888da60d90f4962508c
Mr. Error <122160056+101rror@users.noreply.github.com>
2023-01-08 10:57:31 +0600

    Add files via upload

---
+++ Factorial using Recursion.cpp
@@ -0,0 +1,66 @@
+///Factorial using Recursion
+
+/**
+*
+*   author :: 101rror
+*
+**/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define MAX 100
+#define infinity 1000000000000000LL
+#define all(x) (x).begin(),(x).end()
+#define FastRead ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define mod(x,m) ((x%m)+m)%m;                               //101rror
+#define fastread() (ios_base:: sync_with_stdio(false),cin.tie(NULL))
+
+#define T while(t--)
+#define for2(i,a,b) for(i=a;i>=b;i--)
+#define for3(i,a,b) for(i=a;i<=b;i=i+2)
+#define for1(i,a,b) for(i=a;i<=b;i=i+1)
+#define for4(i,a,b) for(i=a;i>=b;i=i-2)
+
+#define fi first
+#define se second
+#define pb push_back
+#define eb emplace_back
+
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+
+
+int Factorial(int num)
+{
+    if(num > 1)
+    {
+        return (num * Factorial(num - 1));
+    }
+    else
+    {
+         return 1;
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    fastread();
+
+
+    int num;
+    cin >> num;
+
+    cout << endl;
+    cout << num << " Factorial is : " << Factorial(num);
+    cout << endl;
+
+
+    return 0;
+}
