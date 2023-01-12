1e237a5d79d3adbcfd3bdde53fc98ef9f94346a1
Mr. Error <122160056+101rror@users.noreply.github.com>
2023-01-12 21:52:31 +0600

    Add files via upload

---
+++ Largest element of an Array using STL.cpp
@@ -0,0 +1,64 @@
+///Largest element of an Array using STL
+
+/**
+*
+*   author :: 101rror
+*
+**/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define MAX 100
+#define infinity 1000000000000000LL
+#define all(x) (x).begin(),(x).end()
+#define FastRead ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define mod(x,m) ((x%m)+m)%m;                               //101rror
+#define fastread() (ios_base:: sync_with_stdio(false),cin.tie(NULL))
+
+#define T while(t--)
+#define for2(i,a,b) for(i=a;i>=b;i--)
+#define for3(i,a,b) for(i=a;i<=b;i=i+2)
+#define for1(i,a,b) for(i=a;i<=b;i=i+1)
+#define for4(i,a,b) for(i=a;i>=b;i=i-2)
+
+#define fi first
+#define se second
+#define pb push_back
+#define eb emplace_back
+
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    fastread();
+
+    int i, num;
+    cin >> num;
+
+    int array[num];
+
+    for(i = 0; i < num; i++)
+    {
+        cin >> array[i];
+    }
+
+    sort(array, array + num);
+
+    int Largest = *max_element(array, array + num);
+
+    cout << endl;
+    cout << "The Largest Element is : " << Largest;
+    cout << endl;
+
+
+    return 0;
+}
